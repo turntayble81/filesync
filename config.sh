ec6abad0a26e407368eb065ed02d15a22c491a95
turntayble81 <haigpapaghanian@rocketloans.com>
2018-05-07 17:32:10 -0400

    Added sync script and config file

---
+++ config.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#POLLING FREQUENCY (IN SECONDS)
+POLLING_TIME=0.5;
+
+#ABSOLUTE PATH TO DIRECTORY TO WATCH. NO TRAILING SLASH
+WATCH_DIR="/path/to/dir";
+
+#SSH_USER
+SSH_USER="username";
+
+#SSH HOST
+SSH_HOST="server.hostname.com";
+
+#SSH PORT
+SSH_PORT=22;
+
+#SSH_DIR RELATIVE TO SSH USER HOME DIR
+SSH_DIR="dest-dir";
+
+#EXCLUDE PATTERNS
+EXCLUDE=( "*node_modules*" "*.git*");
---
+++ sync.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#PATH TO THIS SCRIPT
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+
+#SOURCE CONFIG FILE
+. "$SCRIPT_DIR/config.sh";
+
+#BUILD EXCLUSION STRINGS
+FIND_EXCLUDE=();
+RSYNC_EXCLUDE="";
+for i in "${EXCLUDE[@]}"
+do
+   FIND_EXCLUDE+=("! -path '$i'");
+   RSYNC_EXCLUDE+=("--exclude='$i'");
+done
+
+#BUILD FIND CMD
+FIND_CMD="
+    find $WATCH_DIR 
+    -type f 
+    \( 
+        -newerct '-$POLLING_TIME seconds' -o 
+        -newermt '-$POLLING_TIME seconds' 
+    \) 
+    ${FIND_EXCLUDE[@]}";
+
+#BUILD RSYNC CMD
+RSYNC_CMD="
+    rsync 
+    --del 
+    -avz 
+    ${RSYNC_EXCLUDE[@]} 
+    -e 'ssh -p$SSH_PORT' $WATCH_DIR/ $SSH_USER@$SSH_HOST:$SSH_DIR
+";
+
+#CREATE SERVER DEST PATH IF DOESN'T EXIST
+echo "Creating remote dir if doesn't exist";
+ssh $SSH_USER@$SSH_HOST -p$SSH_PORT "mkdir -p $SSH_DIR";
+
+#SYNC FILES AS PART OF START UP
+echo "Syncing.";
+eval $RSYNC_CMD;
+
+#START DAEMON
+while [ true ]; do
+
+    #GET LIST OF CREATED/MODIFIED FILES
+    CHANGED_FILES=($(eval $FIND_CMD));
+
+    #IF AT LEAST ONE RELEVANT FILE WAS CREATED/MODIFIED, RSYNC
+    if [ ${#CHANGED_FILES[@]} -gt 0 ]; then
+        echo "${#CHANGED_FILES[@]} file(s) changed. Syncing.";
+        eval $RSYNC_CMD;
+    fi
+
+    sleep $POLLING_TIME;
+done
